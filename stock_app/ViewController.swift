615a3854f7bbc27fc7b34b5d13e880a3fa966c93
gomminjae <gommj0611@naver.com>
2020-08-27 18:31:24 +0900

    Initial Commit

---
+++ stock_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stock_app
+//
+//  Created by 권민재 on 2020/08/27.
+//  Copyright © 2020 gommj_Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
